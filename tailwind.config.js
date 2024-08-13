278b1a87f7e7b1e2c5da4e72f832d2896ac8d0c1
Oskarowski <31699795+Oskarowski@users.noreply.github.com>
2024-08-13 21:41:21 +0200

    persistent tailwind classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
         extend: {},
     },
     plugins: [],
+    safelist: [
+        "bg-blue-600",
+        "bg-blue-700",
+        "bg-red-600",
+        "bg-red-700",
+        "text-white",
+        "rounded",
+        "hover:bg-blue-700",
+        "hover:bg-red-700",
+    ],
 };
 
 // npx tailwindcss -i ./main.css -o ./dist/tailwind.css --watch
