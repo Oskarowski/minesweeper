8d1a8383a8c939260e389882ddb7a3db93059c3f
Oskarowski <31699795+Oskarowski@users.noreply.github.com>
2024-08-13 21:42:33 +0200

    add HTMX and play with it

--- main.go
+++ main.go
@@ -6,25 +6,98 @@ import (
 	"text/template"
 )
 
+type UserInfo struct {
+	Name  string
+	Phone string
+	Email string
+}
+
+type Button struct {
+	Action      string
+	Target      string
+	Label       string
+	ButtonClass string
+	HoverClass  string
+}
+
 func main() {
 	http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.Dir("dist"))))
 
-	templates, err := template.ParseFiles("public/index.html", "public/name_card.html")
+	templates, err := template.ParseGlob("public/*.html")
 
 	if err != nil {
 		panic(fmt.Sprintf("Error parsing template: %v", err))
 	}
 
+	templates, err = templates.ParseGlob("public/components/*.html")
+	if err != nil {
+		panic(fmt.Sprintf("Error parsing components: %v", err))
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		res := map[string]interface{}{
-			"Name":  "Wyndham",
-			"Phone": "8888888",
-			"Email": "skyscraper@gmail.com",
+		revealUserInfoBtn := Button{
+			Action:      "/get-user-info",
+			Target:      "#user-info-container",
+			Label:       "Reveal the User Info",
+			ButtonClass: "bg-blue-600",
+			HoverClass:  "bg-blue-700",
 		}
 
-		err := templates.ExecuteTemplate(w, "index", res)
+		data := struct {
+			RevealUserButton Button
+		}{
+			RevealUserButton: revealUserInfoBtn,
+		}
+
+		err := templates.ExecuteTemplate(w, "index", data)
 		if err != nil {
 			http.Error(w, fmt.Sprintf("Error rendering template: %v", err), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	http.HandleFunc("/get-user-info", func(w http.ResponseWriter, r *http.Request) {
+		userInfo := UserInfo{
+			Name:  "Wyndham",
+			Phone: "8888888",
+			Email: "skyscraper@gmail.com",
+		}
+
+		button := Button{
+			Action:      "/hide-user-info",
+			Target:      "#user-info-container",
+			Label:       "Hide User Info",
+			ButtonClass: "bg-red-600",
+			HoverClass:  "bg-red-700",
+		}
+
+		data := struct {
+			UserInfo       UserInfo
+			HideButtonData Button
+		}{
+			UserInfo:       userInfo,
+			HideButtonData: button,
+		}
+
+		err := templates.ExecuteTemplate(w, "user_info_card", data)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error rendering template: %v", err), http.StatusInternalServerError)
+			return
+		}
+	})
+
+	http.HandleFunc("/hide-user-info", func(w http.ResponseWriter, r *http.Request) {
+		button := Button{
+			Action:      "/get-user-info",
+			Target:      "#user-info-container",
+			Label:       "Reveal the User Info",
+			ButtonClass: "bg-blue-600",
+			HoverClass:  "bg-blue-700",
+		}
+
+		err := templates.ExecuteTemplate(w, "button", button)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error rendering button: %v", err), http.StatusInternalServerError)
 			return
 		}
 	})
