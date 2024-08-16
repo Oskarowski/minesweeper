6cafc5aa10e521c77f50f516b4df9682217431e6
Oskarowski <31699795+Oskarowski@users.noreply.github.com>
2024-08-16 19:19:08 +0200

    mv endpoints to separate handlers

--- main.go
+++ main.go
@@ -2,105 +2,87 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"strconv"
 	"text/template"
 )
 
-type UserInfo struct {
-	Name  string
-	Phone string
-	Email string
+// Package-level variable to store the parsed templates
+var templates *template.Template
+
+func init() {
+	var err error
+
+	// Parse templates from the "public" and "public/components" directories
+	templates, err = template.ParseGlob("public/*.html")
+	if err != nil {
+		log.Fatalf("Error parsing templates: %v", err)
+	}
+
+	templates, err = templates.ParseGlob("public/components/*.html")
+	if err != nil {
+		log.Fatalf("Error parsing components: %v", err)
+	}
+
+	// Log the templates that have been parsed
+	for _, tmpl := range templates.Templates() {
+		log.Printf("Parsed template: %s", tmpl.Name())
+	}
 }
 
-type Button struct {
-	Action      string
-	Target      string
-	Label       string
-	ButtonClass string
-	HoverClass  string
+func startGameHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, "Unable to process form", http.StatusBadRequest)
+		return
+	}
+
+	// get the form data
+	gridSize, gridSizeErr := strconv.Atoi(r.FormValue("grid-size"))
+	minesAmount, minesAmountErr := strconv.Atoi((r.FormValue("mines-amount")))
+
+	if gridSizeErr != nil || minesAmountErr != nil {
+		http.Error(w, "Invalid input values", http.StatusBadRequest)
+		return
+	}
+
+	if gridSize <= 0 || minesAmount <= 0 {
+		http.Error(w, "The grid size and mines amount must be greater than 0", http.StatusUnprocessableEntity)
+		return
+	}
+
+	fmt.Printf("Grid size: %v Mines amount: %v", gridSize, minesAmount)
+
+	responseData := struct {
+		GridSize    int
+		MinesAmount int
+	}{
+		GridSize:    gridSize,
+		MinesAmount: minesAmount,
+	}
+
+	err := templates.ExecuteTemplate(w, "game_grid", responseData)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error rendering template: %v", err), http.StatusInternalServerError)
+		return
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "index", nil)
+
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error rendering template: %v", err), http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
 	http.Handle("/dist/", http.StripPrefix("/dist/", http.FileServer(http.Dir("dist"))))
 
-	templates, err := template.ParseGlob("public/*.html")
+	http.HandleFunc("/", indexHandler)
 
-	if err != nil {
-		panic(fmt.Sprintf("Error parsing template: %v", err))
-	}
-
-	templates, err = templates.ParseGlob("public/components/*.html")
-	if err != nil {
-		panic(fmt.Sprintf("Error parsing components: %v", err))
-	}
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		revealUserInfoBtn := Button{
-			Action:      "/get-user-info",
-			Target:      "#user-info-container",
-			Label:       "Reveal the User Info",
-			ButtonClass: "bg-blue-600",
-			HoverClass:  "bg-blue-700",
-		}
-
-		data := struct {
-			RevealUserButton Button
-		}{
-			RevealUserButton: revealUserInfoBtn,
-		}
-
-		err := templates.ExecuteTemplate(w, "index", data)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error rendering template: %v", err), http.StatusInternalServerError)
-			return
-		}
-	})
-
-	http.HandleFunc("/get-user-info", func(w http.ResponseWriter, r *http.Request) {
-		userInfo := UserInfo{
-			Name:  "Wyndham",
-			Phone: "8888888",
-			Email: "skyscraper@gmail.com",
-		}
-
-		button := Button{
-			Action:      "/hide-user-info",
-			Target:      "#user-info-container",
-			Label:       "Hide User Info",
-			ButtonClass: "bg-red-600",
-			HoverClass:  "bg-red-700",
-		}
-
-		data := struct {
-			UserInfo       UserInfo
-			HideButtonData Button
-		}{
-			UserInfo:       userInfo,
-			HideButtonData: button,
-		}
-
-		err := templates.ExecuteTemplate(w, "user_info_card", data)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error rendering template: %v", err), http.StatusInternalServerError)
-			return
-		}
-	})
-
-	http.HandleFunc("/hide-user-info", func(w http.ResponseWriter, r *http.Request) {
-		button := Button{
-			Action:      "/get-user-info",
-			Target:      "#user-info-container",
-			Label:       "Reveal the User Info",
-			ButtonClass: "bg-blue-600",
-			HoverClass:  "bg-blue-700",
-		}
-
-		err := templates.ExecuteTemplate(w, "button", button)
-		if err != nil {
-			http.Error(w, fmt.Sprintf("Error rendering button: %v", err), http.StatusInternalServerError)
-			return
-		}
-	})
+	http.HandleFunc("/start-game", startGameHandler)
 
 	fmt.Println("Server is listening on port 8080...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
